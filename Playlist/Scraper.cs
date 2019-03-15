23f3dc7b9a70f047a686745907308fdf7d91250b
ricmtz <ricmtz15@gmail.com>
2019-03-14 23:50:36 -0600

    Test of ironwebscraper

---
+++ Playlist/Scraper.cs
@@ -0,0 +1,31 @@
+﻿using IronWebScraper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playlist
+{
+    class Scraper : WebScraper
+    {
+        public override void Init()
+        {
+            License.LicenseKey = "LicenseKey";
+            this.LoggingLevel = WebScraper.LogLevel.All;
+            //this.ObeyRobotsDotTxt = false;
+            this.Request("https://www.youtube.com/results?search_query=GTX+1660", Parse);
+        }
+
+        public override void Parse(Response response)
+        {
+            Console.WriteLine("scraping");
+            var videos = new List<string>();
+            foreach (var item in response.XPath("//a[@id='video-title']")){
+                string link = item.Attributes["href"];
+                videos.Add(link);
+            }
+            Scrape(videos, "Videos.json");
+        }
+    }
+}
